b1f6a1ff191cf9b8e76e879b15dc4e4c960c1a82
sabina12243 <sharmasabina466@gmail.com>
2025-09-04 18:59:01 +0300

    hi

---
+++ leap.py
@@ -0,0 +1,8 @@
+try:
+    year = int(input("Enter a year: "))
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+except ValueError:
+    print("Invalid input. Please enter a valid year (e.g., 2024).")
